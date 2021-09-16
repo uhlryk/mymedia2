4107fcd610c63e0e36766e3dfb11036e0cf08774
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-09-16 23:26:16 +0200

    react init

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h2>Hello from ReactA!</h2>, document.getElementById("root"));
+}
+
+render();
