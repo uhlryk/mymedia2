6fb2c61c658237ebab4f8cdd9d2e40e223af6a8a
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2021-12-27 20:29:46 +0100

    add material ui

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Button from '@mui/material/Button';
 export default function App() {
   return (
-    <div className="app">Hello</div>
+    <div className="app">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
   );
 }
