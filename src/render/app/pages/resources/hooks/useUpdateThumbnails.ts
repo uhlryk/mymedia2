f17354597336ab6495a4ee4436e5d779916d3c9d
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-02 21:29:58 +0100

    fix breaking async gen thumb creator function

--- src/render/app/pages/resources/hooks/useUpdateThumbnails.ts
+++ src/render/app/pages/resources/hooks/useUpdateThumbnails.ts
@@ -3,42 +3,44 @@ import { IResource } from '../../../../../shared/IResource';
 import { requestThumbnails } from './requestThumbnails';
 
 type IInputUseUpdateThumbanails = {
-  projectFolderPath: string;
-  isLoading: boolean;
-  resourceList: IResource[];
-  setResourceList: Dispatch<SetStateAction<IResource[]>>;
+    projectFolderPath: string;
+    isLoading: boolean;
+    resourceList: IResource[];
+    setResourceList: Dispatch<SetStateAction<IResource[]>>;
 };
 export const useUpdateThumbanails = ({
-  projectFolderPath,
-  isLoading,
-  resourceList,
-  setResourceList,
+    projectFolderPath,
+    isLoading,
+    resourceList,
+    setResourceList,
 }: IInputUseUpdateThumbanails): void => {
-  useEffect(() => {
-    let stopProcess = false;
-    const updateThumbnails = async () => {
-      if (!isLoading) {
-        const asyncGenRequestThumbnails = requestThumbnails({
-          projectFolderPath,
-          resourceList,
-          stopProcess,
-        });
-        for await (const updatedResource of asyncGenRequestThumbnails) {
-          const resourceIndex = resourceList.findIndex(
-            (resource) => resource.id === updatedResource.id
-          );
-          if (resourceIndex !== -1) {
-            resourceList[resourceIndex] = updatedResource;
-          }
-          const updatedResourceList = resourceList.slice();
-          updatedResourceList[resourceIndex] = updatedResource;
-          setResourceList(updatedResourceList);
-        }
-      }
-    };
-    updateThumbnails();
-    return () => {
-      stopProcess = true;
-    };
-  }, [isLoading]);
+    useEffect(() => {
+        let stopProcess = false;
+        const updateThumbnails = async () => {
+            if (!isLoading) {
+                const asyncGenRequestThumbnails = requestThumbnails({
+                    projectFolderPath,
+                    resourceList
+                });
+                for await (const updatedResource of asyncGenRequestThumbnails) {
+                    if (stopProcess) {
+                        break;
+                    }
+                    const resourceIndex = resourceList.findIndex(
+                        (resource) => resource.id === updatedResource.id
+                    );
+                    if (resourceIndex !== -1) {
+                        resourceList[resourceIndex] = updatedResource;
+                    }
+                    const updatedResourceList = resourceList.slice();
+                    updatedResourceList[resourceIndex] = updatedResource;
+                    setResourceList(updatedResourceList);
+                }
+            }
+        };
+        updateThumbnails();
+        return () => {
+            stopProcess = true;
+        };
+    }, [isLoading]);
 };
