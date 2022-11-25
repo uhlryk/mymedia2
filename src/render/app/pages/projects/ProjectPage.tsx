5b2c3091c6edec1e26f4c154040c759bc7aebfca
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-25 13:54:12 +0100

    split project list

--- src/render/app/pages/projects/ProjectPage.tsx
+++ src/render/app/pages/projects/ProjectPage.tsx
@@ -1,21 +1,11 @@
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FolderOpenIcon from '@mui/icons-material/FolderOpen';
-import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 import React, { ReactElement, ReactNode, useContext, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import { AppStore } from '../../store/useAppStore';
 import { AppStoreContext } from '../../store/AppStoreContextProvider';
 import { setProject } from '../../store/appStoreActions';
 import { useProjectList } from './hooks/useProjectList';
 import { removeProjectFromList } from './api/removeProjectFromList';
+import { ProjectList } from './components/ProjectList';
 
 export const ProjectPage = (): ReactElement => {
   console.log(`[SelectProject] start `);
@@ -34,65 +24,8 @@ export const ProjectPage = (): ReactElement => {
     });
   };
 
-  const tableRows = projectList.map((project) => (
-    <TableRow hover key={project.id}>
-      <TableCell
-        component="th"
-        scope="row"
-        onClick={() => onSelectProject(project.id)}
-      >
-        {project.name}
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        onClick={() => onSelectProject(project.id)}
-      >
-        {project.folderPath}
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        <IconButton
-          aria-label="open"
-          onClick={() => onSelectProject(project.id)}
-        >
-          <FolderOpenIcon />
-        </IconButton>
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        <IconButton aria-label="rename">
-          <DriveFileRenameOutlineIcon />
-        </IconButton>
-      </TableCell>
-      <TableCell component="th" scope="row" align="center">
-        <IconButton
-          aria-label="delete"
-          onClick={() => onRemoveProject(project.id)}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </TableCell>
-    </TableRow>
-  ));
 
   return (
-    <TableContainer
-      component={Paper}
-      sx={{
-        padding: '30px',
-      }}
-    >
-      <Table stickyHeader aria-label="simple table">
-        <TableHead>
-          <TableRow hover>
-            <TableCell>Project Name</TableCell>
-            <TableCell>Project Path</TableCell>
-            <TableCell align="center">Open</TableCell>
-            <TableCell align="center">Rename</TableCell>
-            <TableCell align="center">Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>{tableRows}</TableBody>
-      </Table>
-    </TableContainer>
-  );
+    <ProjectList projectList={projectList} onSelectProject={onSelectProject} onRemoveProject={onRemoveProject} />
+  )
 };
