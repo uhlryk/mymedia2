e0b2614febe6fcb525e12abe49672129ea921349
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-27 19:54:31 +0100

    show tags

--- src/render/app/pages/projects/components/ProjectList.tsx
+++ src/render/app/pages/projects/components/ProjectList.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
--- src/render/app/pages/resources/pages/tagsList/TagsListPage.tsx
+++ src/render/app/pages/resources/pages/tagsList/TagsListPage.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 import { useAppSelector } from '../../../../store/store';
 import { selectTagsList } from '../../store/resourcesSlice';
 
 export const TagsListPage = () => {
     const tagList = useAppSelector(selectTagsList);
     console.log(tagList);
+
+    const tableRows = tagList.map((tag) => (
+        <TableRow hover key={tag.id}>
+            <TableCell
+                component="th"
+                scope="row"
+            >
+                {tag.name}
+            </TableCell>
+            <TableCell
+                component="th"
+                scope="row"
+            >
+                {tag.parentId ? "" : ""}
+            </TableCell>
+            <TableCell component="th" scope="row" align="center">
+                <IconButton
+                    aria-label="open"
+                >
+                    <FolderOpenIcon />
+                </IconButton>
+            </TableCell>
+            <TableCell component="th" scope="row" align="center">
+                <IconButton aria-label="rename">
+                    <DriveFileRenameOutlineIcon />
+                </IconButton>
+            </TableCell>
+            <TableCell component="th" scope="row" align="center">
+                <IconButton
+                    aria-label="delete"
+                >
+                    <DeleteIcon />
+                </IconButton>
+            </TableCell>
+        </TableRow>
+    ));
+
     return (
-        <h1>Hello TagsList page</h1>
-    )
+        <TableContainer
+            component={Paper}
+            sx={{
+                padding: '30px',
+            }}
+        >
+            <Table stickyHeader aria-label="simple table">
+                <TableHead>
+                    <TableRow hover>
+                        <TableCell>Project Name</TableCell>
+                        <TableCell>Project Path</TableCell>
+                        <TableCell align="center">Open</TableCell>
+                        <TableCell align="center">Rename</TableCell>
+                        <TableCell align="center">Delete</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>{tableRows}</TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
