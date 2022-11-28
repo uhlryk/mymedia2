7e41c79725c2b42d7305cf501217cb1d7b10b26e
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-28 21:34:17 +0100

    all videos have same number of thumbs

--- src/main/project/utils/thumbnails/getFramePerSec.ts
+++ src/main/project/utils/thumbnails/getFramePerSec.ts
@@ -1,11 +1,6 @@
 // if duration is < 120s there should be 4 thumbnails, otherwise thumbnail per 30s
 export const getFramePerSec = (duration: number): number => {
-    let framePerSec
-    if (duration < 120) {
-        framePerSec = 4 / duration;
-    } else {
-        framePerSec = 1 / 30;
-    }
+    const framePerSec = 4 / duration;
 
     return framePerSec;
 }
